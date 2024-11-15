dc217fe30c7d147a021451836d877a1316a0f4a6
mk-938 <mk938@kent.ac.uk>
2024-11-15 11:14:35 +0000

    hi

---
+++ Task 1.py
@@ -0,0 +1,8 @@
+import sys
+#print("Total arguments:", len(sys.argv))
+#print("Script name:", sys.argv[0])
+#if len(sys.argv) > 1:
+#    print("Additional arguments:")
+#    for arg in sys.argv[1:]:
+#        print(arg)
+print (f"Hello, {sys.argv[1]}!")
